c711b406968491df09dec7a1e0391ae75ba2e7e4
nikolas <kazanjian.n@brainrocket.com>
2026-02-05 15:37:44 +0200

    interfaces for movie pick service and search controller

--- Movie Mate Game/Assets/Scripts/Game/IMoviePickService.cs
+++ Movie Mate Game/Assets/Scripts/Game/IMoviePickService.cs
@@ -1,7 +1,10 @@
+using Cysharp.Threading.Tasks;
+using DefaultNamespace.Models;
+
 namespace DefaultNamespace.Game
 {
     public interface IMoviePickService
     {
-        
+        UniTask<DetailsSuperlistModel> GetValidGameMovieAsync();
     }
 }
--- Movie Mate Game/Assets/Scripts/Game/ISearchController.cs
+++ Movie Mate Game/Assets/Scripts/Game/ISearchController.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+
 namespace DefaultNamespace.Game
 {
     public interface ISearchController
     {
-        
+        void StartSearch(string search);
+        void LoadNextPage();
+        void ResetSearchState();
+        void SetFilter(HashSet<int> guessedIds);
+        void Dispose();
     }
 }
